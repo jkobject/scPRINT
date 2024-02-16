5821a5820a88b84ea93fa3099fb2ccc735de8797
maestro-jk <jkalfon@pasteur.fr>
2024-02-16 14:22:44 +0100

    trying out

--- slurm/submit.sh
+++ slurm/submit.sh
@@ -1,11 +1,12 @@
 # (submit.sh)
 #!/bin/bash -l
 
+
 # SLURM SUBMIT SCRIPT
 #SBATCH -q gpu
 #SBATCH -p gpu
-#SBATCH --nodes=1             # This needs to match Trainer(num_nodes=...)
-#SBATCH --gres=gpu:1
+#SBATCH --nodes=$1             # This needs to match Trainer(num_nodes=...)
+#SBATCH --gres=gpu:$2
 #SBATCH --ntasks-per-node=1   # This needs to match Trainer(devices=...)
 #SBATCH --gmem=20G
 #SBATCH --time=0-02:00:00
@@ -23,9 +24,10 @@ export PYTHONFAULTHANDLER=1
 
 # might need the latest CUDA
 # module load NCCL/2.4.7-1-cuda.10.0
+conda activate scprint && module load cuda/11.8 && lamin load scprint
 
 # run script from above
-srun python3 __main__.py --config config/pretrain.yaml --trainer.devices 1
+srun python3 scprint/__main__.py fit --config config/pretrain.yaml --trainer.devices $1 --trainer.logger.offline True --data.num_workers $2 --trainer.strategy "ddp" --trainer.precision "16-true"
 
 
 # 90 seconds before training ends
