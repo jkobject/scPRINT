7d4650b29815e6cb79e6305837785e1178d52432
jkobject <jkobject@gmail.com>
2024-03-26 16:10:15 +0100

    continuing / finishing bench and extractions of GRNs

--- scprint/tasks/grn.py
+++ scprint/tasks/grn.py
@@ -13,26 +13,9 @@ from anndata import AnnData
 
 from scprint.utils.sinkhorn import SinkhornDistance
 from scprint.utils import load_genes
+from scprint.utils import load_genes
 
-from grnndata import GRNAnnData, from_anndata
-
-import umap
-import hdbscan
-
-from matplotlib import pyplot as plt
-import seaborn as sns
-import numpy as np
-from .tmfg import tmfg
-import networkx as nx
-import scipy.sparse
-import os.path
-
-import pandas as pd
-
-from omnipath.interactions import AllInteractions
-from omnipath.requests import Annotations
-
-FILEDIR = os.path.dirname(os.path.realpath(__file__))
+from grnndata import GRNAnnData
 
 
 class GRNfer:
@@ -49,16 +32,8 @@ class GRNfer:
         ],
         cell_type_col="cell_type",
         model_name: str = "scprint",
-        how: str = "random expr",  # random expr, most var withing, most var across, given
-        preprocess="softmax",  # sinkhorn, softmax, none
-        head_agg="mean",  # mean, sum, none
-        cell_agg="mean",  # mean, consensus, none
-        filtration="thresh",  # thresh, top-k, mst, known, none
-        k=10,
-        known_grn=None,
-        doplot=True,
-        max_cells=0,
-        forward_mode="none",
+        preprocess="sinkhorn",
+        cell_type_col="cell_type",
         genes: list = [],
         loc="./",
     ):
@@ -100,49 +75,15 @@ class GRNfer:
         self.cell_agg = cell_agg
         self.doplot = doplot
         self.genes = genes
-        self.forward_mode = forward_mode
-        self.k = k
-        self.loc = loc
-        self.known_grn = known_grn
-        self.head_agg = head_agg
-        self.max_cells = max_cells
-        self.curr_genes = None
         self.trainer = Trainer(precision=precision)
         # subset_hvg=1000, use_layer='counts', is_symbol=True,force_preprocess=True, skip_validate=True)
 
     def __call__(self, layer, cell_type=None):
         # Add at least the organism you are working with
-        self.curr_genes = None
-        self.model.pred_log_adata = False
-        self.model.get_attention_layer = layer if type(layer) is list else [layer]
-        subadata = self.predict(cell_type)
-        adjacencies = self.aggregate(self.model.mean_attn[0])
-        if self.head_agg == "none" or self.cell_agg == "none":
-            return [
-                self.save(self.filter(subadj), subadata, self.loc + str(i))
-                for i, subadj in enumerate(adjacencies)
-            ]
-        elif self.cell_agg == "consensus":
-            return self.save(
-                np.sum(
-                    np.array([self.filter(subadj)[8:, 8:] for subadj in adjacencies]),
-                    axis=0,
-                ),
-                subadata,
-                self.loc,
-            )
-        else:
-            return self.save(self.filter(adjacencies)[8:, 8:], subadata, self.loc)
-
-    def predict(self, cell_type):
         if cell_type is not None:
-            subadata = (
-                self.adata[self.adata.obs[self.cell_type_col] == cell_type].copy()[
-                    : self.max_cells
-                ]
-                if self.max_cells
-                else self.adata[self.adata.obs[self.cell_type_col] == cell_type].copy()
-            )
+            subadata = self.adata[
+                self.adata.obs[self.cell_type_col] == cell_type
+            ].copy()[:1]
         else:
             subadata = (
                 self.adata.copy()[: self.max_cells]
@@ -190,66 +131,13 @@ class GRNfer:
             num_workers=self.num_workers,
             shuffle=False,
         )
-        self.model.predict_mode = self.forward_mode
+        self.model.get_attention_layer = layers
         self.trainer.predict(self.model, dataloader)
-        return subadata
 
-    def aggregate(self, attn):
-        if self.cell_agg == "mean":
-            if self.how == "random expr":
-                attn = torch.zeros(
-                    [len(self.model.genes) + len(self.model.labels) + 2]
-                    + list(self.model.mean_attn[0].shape[2:])
-                )
-                div = torch.zeros(
-                    [len(self.model.genes) + len(self.model.labels) + 2]
-                    + list(self.model.mean_attn[0].shape[2:])
-                )
-
-                for i in range(self.model.mean_attn[0].shape[0]):
-                    loc = torch.cat(
-                        [torch.Tensor([r for r in range(8)]), self.model.pos[i]]
-                    ).int()
-                    attn[loc, :, :, :] += self.model.mean_attn[0][i].detach().to("cpu")
-                    div[loc, :, :, :] += 1
-                attn = attn / div
-                goodloc = div.sum((1, 2, 3)) > 0
-                attn = attn[goodloc]
-                self.curr_genes = np.array(self.curr_genes)[goodloc[8:]]
-            else:
-                attn = self.model.mean_attn[0].mean(0)
-                self.curr_genes = [i for i in self.curr_genes if i in self.model.genes]
-            if self.doplot:
-                sns.set(
-                    style="white", context="poster", rc={"figure.figsize": (14, 10)}
-                )
-                fit = umap.UMAP()
-                mm = fit.fit_transform(attn[:, 0, 0, :].detach().cpu().numpy())
-                labels = hdbscan.HDBSCAN(
-                    min_samples=10,
-                    min_cluster_size=100,
-                ).fit_predict(mm)
-                plt.scatter(mm[:, 0], mm[:, 1], c=labels)
-                plt.title(f"Qs @H{0}")
-                plt.show()
-                mm = fit.fit_transform(attn[:, 1, 0, :].detach().cpu().numpy())
-                labels = hdbscan.HDBSCAN(
-                    min_samples=10,
-                    min_cluster_size=100,
-                ).fit_predict(mm)
-                plt.scatter(mm[:, 0], mm[:, 1], c=labels)
-                plt.title(f"Ks @H{0}")
-                plt.show()
-            attn = attn[:, 0, :, :].permute(1, 0, 2) @ attn[:, 1, :, :].permute(1, 2, 0)
-        elif self.cell_agg == "consensus" or self.cell_agg == "none":
-            attn = self.model.mean_attn[0]
-            attn = attn[:, :, 0, :, :].permute(2, 0, 1, 3) @ attn[
-                :, :, 1, :, :
-            ].permute(2, 0, 3, 1)
-            self.curr_genes = [i for i in self.curr_genes if i in self.model.genes]
-        else:
-            raise ValueError("cell_agg must be one of 'mean', 'consensus' or 'none'")
-        # return attn
+        attn = self.model.mean_attn[0][0:][:, 0, :, :].permute(
+            1, 0, 2
+        ) @ self.model.mean_attn[0][0:][:, 1, :, :].permute(1, 2, 0)
+        return attn
         scale = self.model.mean_attn[0].shape[-1] ** -0.5
         attn = attn * scale
         if self.preprocess == "sinkhorn":
@@ -265,64 +153,51 @@ class GRNfer:
         else:
             raise ValueError("preprocess must be one of 'sinkhorn', 'softmax', 'none'")
 
-        if self.head_agg == "mean":
-            if len(attn.shape) == 4:
-                attn = attn.mean(0)
-            attn = attn.mean(0).detach().cpu().numpy()
-        elif self.head_agg == "max":
-            if len(attn.shape) == 4:
-                attn = attn.mean(0)
-            import pdb
+        if agg == "mean":
+            a = a.mean(0).detach().cpu().numpy()
+        elif agg == "max":
+            a = a.max(0)[0].detach().cpu().numpy()
+        elif agg == "None":
+            a = a.detach().cpu().numpy()
+            grns = []
+            for suba in a:
+                print(
+                    "avg link count: "
+                    + str((suba > (1 / attn.shape[-1])).sum() / attn.shape[-1])
+                )
+                suba[suba < (1 / a.shape[-1])] = 0
+                grn = GRNAnnData(
+                    subadata[:, col.accepted_genes[self.organisms[0]]][
+                        :, col.to_subset[self.organisms[0]]
+                    ].copy(),
+                    grn=suba,
+                )
+                top_central_genes = utils.get_centrality(grn)
+                grn.var.loc[[i[0] for i in top_central_genes], "symbol"]
 
-            pdb.set_trace()
-            attn = attn.max(0)[0].detach().cpu().numpy()
-        elif self.head_agg == "none":
-            attn = attn.detach().cpu().numpy()
-            if self.cell_agg == "none":
-                # flatten the first 2 dims
-                attn = attn.view(
-                    attn.shape[0] * attn.shape[1], attn.shape[2], attn.shape[3]
+                grn.var_names = grn.var["symbol"]
+                grn.var["TFs"] = [
+                    True if i in utils.TF else False for i in grn.var_names
+                ]
+
+                grn.var = grn.var.drop(
+                    columns=["stable_id", "created_at", "updated_at"]
                 )
         else:
-            raise ValueError("head_agg must be one of 'mean', 'max' or 'None'")
-        return attn
-
-    def filter(self, adj, gt=None):
+            raise ValueError("agg must be one of 'mean', 'max' or 'None'")
+        print(
+            "avg link count: " + str((a > (1 / attn.shape[-1])).sum() / attn.shape[-1])
+        )
         if self.filtration == "thresh":
-            adj[adj < (1 / adj.shape[-1])] = 0
-            # res = (adj != 0).sum()
-            # if res / adj.shape[0] ** 2 < 0.01:
-            #   adj = scipy.sparse.csr_matrix(adj)
+            a[a < (1 / a.shape[-1])] = 0
         elif self.filtration == "none":
             pass
         elif self.filtration == "top-k":
-            args = np.argsort(adj)
-            adj[np.arange(adj.shape[0])[:, None], args[:, : -self.k]] = 0
-            adj = scipy.sparse.csr_matrix(adj)
-        elif self.filtration == "known" and gt is not None:
-            gt = gt.reindex(sorted(gt.columns), axis=1)
-            gt = gt.reindex(sorted(gt.columns), axis=0)
-            gt = gt[gt.index.isin(self.curr_genes)].iloc[
-                :, gt.columns.isin(self.curr_genes)
-            ]
-
-            loc = np.isin(self.curr_genes, gt.index)
-            self.curr_genes = np.array(self.curr_genes)[loc]
-            adj = adj[8:, 8:][loc][:, loc]
-            adj[gt.values != 1] = 0
-            adj = scipy.sparse.csr_matrix(adj)
-        elif self.filtration == "tmfg":
-            adj = nx.to_scipy_sparse_array(tmfg(adj))
-        elif self.filtration == "mst":
+            np.argsort(a)
+        elif self.filtration == "known":
             pass
         else:
             raise ValueError("filtration must be one of 'thresh', 'none' or 'top-k'")
-        res = (adj != 0).sum() if self.filtration != "none" else adj.shape[0] ** 2
-        print(f"avg link count: {res}, sparsity: {res / adj.shape[0] ** 2}")
-        return adj
-
-    def save(self, grn, subadata, loc="./"):
-        res = (grn != 0).sum() if self.filtration != "none" else grn.shape[0] ** 2
         grn = GRNAnnData(
             subadata[:, subadata.var.index.isin(self.curr_genes)].copy(),
             grn=grn,
