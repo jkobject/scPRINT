90add4e90f56c96503f4af6571d59935dae64d7a
jkobject <jkobject@gmail.com>
2024-09-04 15:49:33 +0200

    lmprovements

--- tests/test_base.py
+++ tests/test_base.py
@@ -6,7 +6,8 @@ import urllib.request
 from scprint.base import NAME
 from scdataloader import Preprocessor
 from scdataloader.utils import populate_my_ontology
-import subprocess
+from scprint.tasks import Denoiser
+from scprint import scPrint
 
 
 def test_base():
@@ -14,12 +15,12 @@ def test_base():
     populate_my_ontology(
         organisms=["NCBITaxon:10090", "NCBITaxon:9606"],
         sex=["PATO:0000384", "PATO:0000383"],
-        celltypes=None,
-        ethnicities=None,
-        assays=None,
-        tissues=None,
-        diseases=None,
-        dev_stages=None,
+        # celltypes=None,
+        # ethnicities=None,
+        # assays=None,
+        # tissues=None,
+        # diseases=None,
+        # dev_stages=None,
     )
     filepath = os.path.join(os.path.dirname(__file__), "test.h5ad")
     ckpt_path = os.path.join(os.path.dirname(__file__), "small.ckpt")
@@ -27,28 +28,29 @@ def test_base():
         url = "https://huggingface.co/jkobject/scPRINT/resolve/main/small.ckpt"
         urllib.request.urlretrieve(url, ckpt_path)
 
-    result = subprocess.run(
-        [
-            "scprint",
-            "denoise",
-            "--ckpt_path",
-            ckpt_path,
-            "--adata",
-            filepath,
-            "--output_filename",
-            "out",
-            "--plot_corr_size",
-            "10",
-            "--species",
-            "NCBITaxon:9606",
-        ],
-        capture_output=True,
-        text=True,
+    adata = sc.read_h5ad(filepath)
+    adata.obs.drop(columns="is_primary_data", inplace=True, errors="ignore")
+    adata.obs["organism_ontology_term_id"] = "NCBITaxon:9606"
+    preprocessor = Preprocessor(
+        do_postp=False,
+        force_preprocess=True,
     )
+    adata = preprocessor(adata)
+    # conf = dict(self.config_init[subcommand])
 
-    exit_code = result.returncode
-    print("stdout:", result.stdout)
-    if exit_code != 0:
-        print("stderr:", result.stderr)
-        assert False, f"scPRINT denoise test failed with exit code {exit_code}"
-    assert exit_code == 0, f"scPRINT denoise test failed with exit code {exit_code}"
+    model = scPrint.load_from_checkpoint(ckpt_path, precpt_gene_emb=None)
+    dn = Denoiser(
+        plot_corr_size=10,
+        batch_size=2,
+        num_workers=1,
+        max_len=300,
+        downsample=0.3,
+        predict_depth_mult=3,
+    )
+    metrics, random_indices, genes, expr_pred = dn(
+        model=model,
+        adata=adata,
+    )
+    print("metrics:")
+    print(metrics)
+    assert metrics["reco2full"] - metrics["noisy2full"] > 0
